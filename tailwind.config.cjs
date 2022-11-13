9852bbd8285582ddd144cc0050cdf04f322eab49
Martin Emmert <martin@martinemmert.de>
2022-11-13 16:22:03 +0100

    :lipstick: Add base layout and imprint
    \

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,25 @@
 /** @type {import('tailwindcss').Config} */
+const { DEFAULT } = require("@tailwindcss/typography");
+
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [
-		require('@tailwindcss/typography')
-	],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        "warm-grey": "#E1E3E3",
+        "vivid-blue": "#27A6EE",
+        "dark-grey": "#2D4857",
+        "light-dark-grey": "#7695A7",
+        "lighter-grey": "#91B5CA",
+      },
+      letterSpacing: {
+        "extra-wide": "0.20rem",
+      },
+    },
+    fontFamily: {
+      sans: ["neue-haas-grotesk-text", "sans-serif"],
+      display: ["neue-haas-grotesk-display", "sans-serif"],
+    },
+  },
+  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
+};
