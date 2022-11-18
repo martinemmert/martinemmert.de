875dac9b136873c791aa80eac944bba03a11cd12
Martin Emmert <martin@martinemmert.de>
2022-11-18 01:20:52 +0100

    :lipstick: Add footer and contact form elements

---
+++ src/components/_theme.js
@@ -0,0 +1,16 @@
+export const inputFocus = [
+  "focus:border-medium-grey",
+  "focus:ring",
+  "focus:ring-primary-blue",
+  "focus:ring-opacity-40",
+];
+
+export const inputTransition = [
+  "transition",
+  "hover:ease-out",
+  "focus:ease-out",
+  "hover:duration-200",
+  "focus:duration-200",
+  "duration-500",
+  "ease-in",
+];
